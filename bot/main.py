6250f80e9cca3d0bce9b687ea743e54f9847d03a
Yosuke Iida <stuiida2002@gmail.com>
2023-10-03 04:41:02 +0900

    [mod] edit wrong codes

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,7 @@ from slack_sdk import WebClient
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-logging.basicConfig(level=logging.DEBUG)``
+logging.basicConfig(level=logging.DEBUG)
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
