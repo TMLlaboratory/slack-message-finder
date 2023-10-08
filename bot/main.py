fd0b90be5242cedb5701f220bda2b5a80b3450ea
K-wakaT <hasukiiq@gmail.com>
2023-10-09 04:51:54 +0900

    [fix] edit-main.py

--- bot/main.py
+++ bot/main.py
@@ -28,30 +28,40 @@ except Exception as e:
 def handle_message_events(body, logger):
     logger.debug(json.dumps(body, indent=4))
 
+    element_length = len(body.get("event", {}).get("blocks", [{}])[0].get("elements", [{}])[0].get("elements", [{}]))
+    file_length = len(body.get("event", {}).get("files", [{}]))
+    if element_length > file_length:
+        loop = element_length
+    else:
+        loop = file_length
+
     user = body.get("event", {}).get("user", "")
     channel = body.get("event", {}).get("channel", "")
     ts = body.get("event", {}).get("ts", "")
     thread_ts = body.get("event", {}).get("thread_ts", "")
-    text = (
+    elements = (
         body.get("event", {})
         .get("blocks", [{}])[0]
         .get("elements", [{}])[0]
-        .get("elements", [{}])[0]
-        .get("text", "")
+        .get("elements", [])
     )
-    url = (
+    files = (
         body.get("event", {})
-        .get("blocks", [{}])[0]
-        .get("elements", [{}])[0]
-        .get("elements", [{}])[0]
-        .get("url", "")
+        .get("files", [])
     )
-    image_name = body.get("event", {}).get("files", [{}])[0].get("name", "")
-    image_url = body.get("event", {}).get("files", [{}])[0].get("url_private", "")
 
-    if connection is not None:
-        try:
-            with connection.cursor() as cursor:
+    for i in range(loop):
+        element = elements[i] if i < len(elements) else {}
+        text = element.get("text", "")
+        url = element.get("url", "")
+
+        file = files[i] if i < len(files) else {}
+        image_name = file.get("name", "")
+        image_url = file.get("url_private", "")
+
+        if connection is not None:
+            try:
+                cursor = connection.cursor()
                 current_datetime = datetime.now()
                 sql = "INSERT INTO messages (channel_id, user_id, ts, thread_ts, text, url, image_name, image_url, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
                 cursor.execute(
@@ -69,13 +79,13 @@ def handle_message_events(body, logger):
                         current_datetime,
                     )
                 )
-            connection.commit()
-            logger.info("データの挿入に成功しました")
-        except Exception as e:
-            logger.info("データの挿入に失敗しました")
-    else:
-        logger.error("データベースへの接続に失敗したため、クエリを実行できません。")
-
+                connection.commit()
+                logger.info("データの挿入に成功しました")
+            except Exception as e:
+                logger.info("データの挿入に失敗しました")
+            cursor.close()
+        else:
+            logger.error("データベースへの接続に失敗したため、クエリを実行できません。")
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
