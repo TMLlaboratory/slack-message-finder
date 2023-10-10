74896fd08fa14869e867e76caaa98e2e10ebcf9d
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2023-10-10 15:53:33 +0900

    modelの消去

--- backend/app/models/channel.rb
+++ backend/app/models/channel.rb
@@ -1,3 +1,3 @@
 class Channel < ApplicationRecord
-    has_many :messages, foreign_key: :channel_id
+    
 end
--- backend/app/models/message.rb
+++ backend/app/models/message.rb
@@ -1,4 +1,3 @@
 class Message < ApplicationRecord
-    belongs_to :user, foreign_key: :user_id
-    belongs_to :channel, foreign_key: :channel_id
+    
 end
