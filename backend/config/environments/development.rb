689e4167e1969925875c721bf58adc34ac16f7f5
K-wakaT <hasukiiq@gmail.com>
2023-10-18 01:27:17 +0900

    [fix] fixrails host name

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "slack-message-finder.coconutml.com"
   config.hosts << "slack-message-finder-api.coconutml.com"
 
   # In the development environment your application's code is reloaded any time
