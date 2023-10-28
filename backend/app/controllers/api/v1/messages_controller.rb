06d0c3e7398626e1841acd2dba87d19c465797a8
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2023-10-29 03:55:12 +0900

    [feat]グラフモーダルにchatGPT-APIを実装する

--- backend/app/controllers/api/v1/messages_controller.rb
+++ backend/app/controllers/api/v1/messages_controller.rb
@@ -4,19 +4,6 @@ class Api::V1::MessagesController < ApplicationController
         render json: messages  # JSONとしてレンダリング
     end
 
-    def index
-        channel_id = params[:channel_id]
-        messages = Message.where(channel_id: channel_id).order(:ts)
-        grouped_messages = group_messages(messages)
-        user_ids = Member.where(channel_id: channel_id).pluck(:user_id)
-        users = User.where(user_id: user_ids).select(:user_id, :email, :image, :real_name)
-        response = {
-            grouped_messages: grouped_messages,
-            users: users
-        }
-        render json: response
-    end
-
     def allmessage
         user_id = params[:user_id]
     
@@ -75,8 +62,68 @@ class Api::V1::MessagesController < ApplicationController
         render json: response
       end
 
-      
-      
+      def aggregate_text
+        user_id = params[:user_id]
+    
+        # user_id からそのユーザーだけの message をすべて取る
+        user_messages = Message.where(user_id: user_id)
+    
+        # すべての message の ts から、月と日を出す
+        message_dates = user_messages.map do |message|
+            # エポックタイムスタンプの小数点以下を切り捨て
+            epoch_timestamp = message.ts.to_i
+    
+            # タイムスタンプをTimeオブジェクトに変換
+            time_object = Time.at(epoch_timestamp).utc
+    
+            # Tokyoタイムゾーンに変換
+            tokyo_time = time_object.in_time_zone('Tokyo')
+    
+            # 日付のみを取得
+            date = tokyo_time.to_date
+            date
+        end.uniq
+    
+        # 一番新しい月と日を取得
+        latest_date = message_dates.max
+        start_date = latest_date - 6.days
+        one_week_dates = (start_date..latest_date)
+    
+        # 日付ごとにメッセージをグループ化
+        grouped_messages = user_messages.group_by do |message|
+            Time.at(message.ts.to_f).in_time_zone("Tokyo").to_date  # タイムゾーンを Tokyo に設定
+        end
+    
+        # 日付でフィルタリングし、ソートしてからテキストを集計
+        aggregated_text = grouped_messages.select { |date, _| one_week_dates.include?(date) }
+            .sort_by { |date, _| date }
+            .map do |date, date_messages|
+                text_for_date = date_messages.map { |message| message.text.gsub("\n", ",") }.join("\n")  # \n を , に変換
+                "#{text_for_date}。"
+            end
+        
+        # 結果を JSON として返す
+        response = {
+            user_id: user_id,
+            aggregated_text: aggregated_text
+        }
+        
+        render json: response
+    end
+    
+    def index
+        channel_id = params[:channel_id]
+        messages = Message.where(channel_id: channel_id).order(:ts)
+        grouped_messages = group_messages(messages)
+        user_ids = Member.where(channel_id: channel_id).pluck(:user_id)
+        users = User.where(user_id: user_ids).select(:user_id, :email, :image, :real_name)
+        response = {
+            grouped_messages: grouped_messages,
+            users: users
+        }
+        render json: response
+    end
+    
     private
 
     def group_messages(messages)
@@ -85,6 +132,9 @@ class Api::V1::MessagesController < ApplicationController
         messages.each do |message|
             ts = message.ts
             thread_ts = message.thread_ts
+
+            new_created_at = convert_ts_to_created_at(ts)
+            message.update(created_at: new_created_at)  # created_atを更新
     
             # thread_ts が nil または空で、ts がグループ化されていない場合、新しい親メッセージ
             if (thread_ts.nil? || thread_ts.empty?) && !grouped.key?(ts)
@@ -106,11 +156,18 @@ class Api::V1::MessagesController < ApplicationController
                 end
             end
         end
-    
         grouped.values
     end
-    
-    # def get_days_message_count(messages)
 
+    private
 
+    def convert_ts_to_created_at(ts)
+        ts_parts = ts.split('.')
+        seconds = ts_parts[0].to_i
+        milliseconds = ts_parts[1][0,3]  # 最初の3文字を取得
+
+        datetime = Time.at(seconds).utc.strftime('%Y-%m-%dT%H:%M:%S')
+        new_created_at = "#{datetime}.#{milliseconds}Z"
+        new_created_at
+    end
 end
--- frontend/app/next.config.js
+++ frontend/app/next.config.js
@@ -2,5 +2,7 @@ module.exports = {
     env: {
         NEXT_PUBLIC_SLACK_ENTERPRISE_TOKEN: process.env.SLACK_ENTERPRISE_TOKEN,
         API_URL: process.env.API_URL,
+        A3RT_API_KEY: process.env.A3RT_API_KEY,
+        GPT_API_KEY: process.env.GPT_API_KEY,
     },
 };
